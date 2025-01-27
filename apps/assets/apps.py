92f05918e2d61d2b370c89cfbf2a33e08c1d153d
Daemonlite <khemikal2016@gmail.com>
2025-01-27 10:35:57 +0000

    asset history

--- apps/assets/apps.py
+++ apps/assets/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.core.signals import setting_changed
 
+
+def my_callback(sender, **kwargs):
+    print("Setting changed!")
 
 class AssetsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.assets"
+
+    def ready(self):
+        from . import signals 
+        setting_changed.connect(my_callback)
+
+
